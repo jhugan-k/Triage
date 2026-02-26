fc1c390605feda6fe071d6ac79013f3fcb814ca3
Jhugan Kartikey <jkartikey.official@gmail.com>
2026-02-26 11:20:02 +0530

    remove prisma config

--- Backend/prisma.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from '@prisma/config';
-
-export default defineConfig({
-  datasource: {
-    // REMOVED: "&channel_binding=require"
-    url: "postgresql://neondb_owner:npg_GP7zk5EhKQML@ep-floral-feather-a1xprotg-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require",
-  },
-});
